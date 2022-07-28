397a0079bfe0159a24c4d4146f7e7dbca1b7d5eb
Suraj <sharmasurajkumar4649@gmail.com>
2022-07-28 16:34:54 +0530

    adding comment in code script for understaning

--- inotebook/back-end/db.js
+++ inotebook/back-end/db.js
@@ -1,6 +1,8 @@
+// connecting mangoose 
 const mangoose=require('mongoose');
-const mongoURI="mongodb://localhost:27017/"
+const mongoURI="mongodb://localhost:27017/" // connect to mangodb local host using this url
 
+// it is basically a function that use as ansure our databse connect i.e for export perpose
 const connectTomango=()=>
 {
    mangoose.connect(mongoURI,()=>{
--- inotebook/back-end/index.js
+++ inotebook/back-end/index.js
@@ -1,6 +1,7 @@
 const connectTomango=require('./db');
 connectTomango();
 
+// script start  -> this script copy from express js server official website for testing purpose
 const express = require('express')
 const app = express()
 const port = 3000
@@ -12,3 +13,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+// script End  ->

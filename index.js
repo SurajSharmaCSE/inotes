086d0c2018991e35cf65ba5e56f1c0725a5f664e
Suraj <sharmasurajkumar4649@gmail.com>
2022-08-13 21:27:49 +0530

    json request save in database basically trial

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ connectTomango();
 const app = express()
 const port = 3000
 
+app.use(express.json()) // midile ware use for deal the request in json format 
+
 // Available routes
-app.use('./api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
